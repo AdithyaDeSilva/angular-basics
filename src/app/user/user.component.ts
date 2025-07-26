a20e8d70678d5cf4206d871a9c3689e4365cd13b
AdithyaDeSilva <adithyadesilva00@gmail.com>
2025-07-26 14:35:03 +0530

    Use type defining and interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+  
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,16 +21,17 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;  
-  @Input({ required: true }) avatar!: string; // Older way instead of signals. accepts input data from the parent component
-  @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter<string>();  //creates a EveneEmitter object, outputs a value from the componen. 
+  @Input({ required: true }) user!: User;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string; // Older way instead of signals. accepts input data from the parent component
+  // @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>(); //creates a EveneEmitter object, outputs a value from the componen.
   // For extra type safety we can add GENERIC type for the emmiting object
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
